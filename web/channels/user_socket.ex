0ac8b8c0ec8e350465be0957f2a3cc6666c6ca8d
Yurii Skrynnykov <truef1s7@gmail.com>
2016-07-30 05:16:37 +0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Yaus.UserSocket do
   # channel "room:*", Yaus.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
     timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
